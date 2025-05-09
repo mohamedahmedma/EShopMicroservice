40875763a0e5e9f1535700553df4f6f35af421bf
Abolhassan <mohamedahmedma345392@gmail.com>
2025-05-09 03:07:50 +0300

    update the docker files

--- E-Shop-microservices/src/Services/Discount/Discount.Grpc/Program.cs
+++ E-Shop-microservices/src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,4 @@
-using Discount.Grpc.Services;
+//using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 if (app.Environment.IsDevelopment())
     app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
