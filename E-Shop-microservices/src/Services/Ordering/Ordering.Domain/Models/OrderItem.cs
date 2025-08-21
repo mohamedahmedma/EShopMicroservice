ea7e384748e6ff96fc57594b98fc242522e446de
mohamedahmedma <mohamedahmedma345392@gmail.com>
2025-08-21 07:15:51 +0300

    Devlop DDD Rich-domain model Entity for Order Entities in Order.Domain Layer and ValueObjects

--- E-Shop-microservices/src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ E-Shop-microservices/src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -5,7 +5,7 @@ public class OrderItem : Entity<OrderItemId>
 {
     internal OrderItem(OrderId orderId , ProductId productId , int quantity , decimal price)
     {
-        Id = OrderItemId.of(Guid.NewGuid());
+        Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
         ProductId = productId;
         Quantity = quantity;
--- E-Shop-microservices/src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
+++ E-Shop-microservices/src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
@@ -4,4 +4,16 @@ namespace Ordering.Domain.ValueObjects;
 public record ProductId
 {
     public Guid Value { get; }
+
+    private ProductId(Guid value) => Value = value;
+
+    public static ProductId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("CustomerId cannot be empty.");
+        }
+        return new ProductId(value);
+    }
 }
