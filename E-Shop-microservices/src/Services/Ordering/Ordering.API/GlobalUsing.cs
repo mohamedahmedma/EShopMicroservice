cbe8cc0d44be731066e6fbb0237c49ad39824d6a
Mohamed Ahmed Mohamed Abolhassan <137604098+mohamedahmedma@users.noreply.github.com>
2025-09-11 02:56:54 +0300

    Finsih The Query Endpoints

--- E-Shop-microservices/src/Services/Ordering/Ordering.API/GlobalUsing.cs
+++ E-Shop-microservices/src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -4,3 +4,5 @@ global using MediatR;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Orders.Commands.CreateOrder;
 global using Ordering.Application.Orders.Commands.UpdateOrder;
+global using BuildingBlocks.Pagination;
+global using Ordering.Application.Orders.Queries.GetOrders;
