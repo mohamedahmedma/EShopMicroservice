187a482513f0a13e93892eaf02ca50eabde61055
Mohamed Ahmed Mohamed Abolhassan <137604098+mohamedahmedma@users.noreply.github.com>
2025-09-10 19:54:56 +0300

    Create Ordering Api and GlobalUsing file and update the dependencyInjection to add carter to services

--- E-Shop-microservices/src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ E-Shop-microservices/src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,16 +1,19 @@
-﻿namespace Ordering.API
+﻿using Carter;
+
+namespace Ordering.API
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApiServices
             (this IServiceCollection services)
         {
+            services.AddCarter();
             return services;
         }
 
         public static WebApplication UseApiSerivces(this WebApplication app)
         {
-            //app.MapCarter();
+            app.MapCarter();
             return app;
         }
     }
