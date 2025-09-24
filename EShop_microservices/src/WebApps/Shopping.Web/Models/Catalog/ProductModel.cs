695db54f1e8cd68f3edee7757bcf6df52ca29481
Mohamed Ahmed Mohamed Abolhassan <137604098+mohamedahmedma@users.noreply.github.com>
2025-09-24 16:33:28 +0300

    Finish Create The Models for Shopping Web Application Microservices consume YarpApi Models : 1- Catalog , 2- Basket , 3-Ordering

---
+++ EShop_microservices/src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Models.Catalog;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public List<string> Category { get; set; } = new();
+}
+
+//wrapper classes
+public record GetProductResponse(IEnumerable<ProductModel> products);
+public record GetProductByCategoryResponse(IEnumerable<ProductModel> products);
+public record GetProductByIdResponse(ProductModel product);
